0c32f84ce2c55a068788fc9ed745053c4a1aee5f
Helton Reis <hrkings@rhaizzing.com>
2019-11-13 08:48:10 -0300

    Initial commit

---
+++ Core/HTMLScrapper.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Text;
+
+namespace HTML_Scrapper.Core
+{
+    public static class HTMLScrapper
+    {
+        public static HtmlDocument GetDocumentFromURL(string html)
+        {
+            try
+            {
+                return new HtmlWeb().Load(html);
+            }
+            catch (Exception e)
+            {
+                ////Logger.Error(e.ToString());
+                return null;
+            }
+        }
+
+        public static HtmlDocument GetDocumentFromURL(string html, Encoding encoding)
+        {
+            try
+            {
+                return new HtmlWeb() { AutoDetectEncoding = false, OverrideEncoding = encoding }.Load(html);
+            }
+            catch (Exception e)
+            {
+                //Logger.Error(e.ToString());
+                return null;
+            }
+        }
+
+        public static HtmlNode GetNode(HtmlDocument html, string xpath)
+        {
+            try
+            {
+                return html.DocumentNode.SelectSingleNode(xpath);
+            }
+            catch (Exception e)
+            {
+                //Logger.Error(e.ToString());
+                return null;
+            }
+        }
+
+        public static string GetElementContent(HtmlDocument html, string xpath)
+        {
+            try
+            {
+                return html.DocumentNode.SelectSingleNode(xpath).InnerText;
+            }
+            catch (Exception e)
+            {
+                //Logger.Error(e.ToString());
+                return null;
+            }
+        }
+
+        public static string GetElementAttribute(HtmlDocument html, string xpath, string attribute)
+        {
+            try
+            {
+                return html.DocumentNode.SelectSingleNode(xpath).GetAttributeValue(attribute, "NA"); ;
+            }
+            catch (Exception e)
+            {
+                //Logger.Error(e.ToString());
+                return null;
+            }
+        }
+    }
+}
---
+++ Helper/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTML_Scrapper.Helper
+{
+    public static class Extensions
+    {
+        public static string DeEntitize(this string entitized)
+        {
+            return HtmlEntity.DeEntitize(entitized);
+        }
+    }
+}
